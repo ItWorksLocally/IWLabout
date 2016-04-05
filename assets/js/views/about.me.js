9d89c6033ee2320998fa440062ee578d2d73b3b4
Lovro Predovan <lovro.predovan@gmail.com>
2016-04-05 18:15:39 +0200

    YOLO

--- assets/js/views/about.me.js
+++ assets/js/views/about.me.js
@@ -16,27 +16,27 @@ define([
         case 'alen':
         data = {
           name: 'Alen',
-          surname: 'Huskanovic',
-          hometown: 'Zadar',
-          company: 'Rentlio',
-          role: 'Backend/Frontend dev',
+          surname: 'Huskanović',
+          hometown: 'Dugo Selo',
+          company: 'Repsly',
+          role: 'Mobile dev',
           image: '/assets/img/alen.jpg',
-          quote: 'Nema labavo.',
-          description: 'All around guy, into web dev, having fun and enjoying low iq music.',
-          github: 'https://github.com/lpredova',
-          twitter: 'https://twitter.com/lovro_p',
-          linkedin: 'https://hr.linkedin.com/in/lovropredovan',
-          education: [{name:'OS Bartula Kasica Zadar',years:'1999-2007'},{name:'Gymnasium of Franjo Petric Zadar',years:'2007-2011'},{name:'Faculty of organization and informatics Varazdin',years:'2011-2016'}],
-          skills:[{name:'PHP',level:'7.5'},{name:'Javascript',level:'5'},{name:'SQL',level:'8'},{name:'Python',level:'6'},{name:'CSS',level:'7.5'},{name:'Java',level:'-9000'}],
+          quote: 'you only have one life, so make it extraordinary',
+          description: 'IT enthusiast and experienced Android developer with big ambitions.',
+          github: 'https://github.com/ahuskano',
+          twitter: 'https://twitter.com/ahuskano',
+          linkedin: 'https://www.linkedin.com/in/huskanovicalen',
+          education: [{name:'OS Dugo Selo',years:'1999-2007'},{name:'High School Dugo Selo/Computer Technician',years:'2007-2011'},{name:'Faculty of organization and informatics Varazdin',years:'2011-2016'}],
+          skills:[{name:'Android',level:'8'},{name:'Java',level:'6'},{name:'SQL',level:'7'},{name:'Python',level:'4'}],
         };
         break;
         case 'igor':
         data = {
           name: 'Igor',
           surname: 'Rinkovec',
-          hometown: 'Zadar',
-          company: 'Rentlio',
-          role: 'Backend/Frontend dev',
+          hometown: 'Varaždin',
+          company: 'Trikoder',
+          role: 'Backend dev',
           image: '/assets/img/igor.jpg',
           quote: 'Nema labavo.',
           description: 'All around guy, into web dev, having fun and enjoying low iq music.',
@@ -54,7 +54,7 @@ define([
           hometown: 'Zadar',
           company: 'Rentlio',
           role: 'Backend/Frontend dev',
-          image: '/assets/img/lovro.JPG',
+          image: 'https://pbs.twimg.com/profile_images/716370302930968578/liP19joH.jpg',
           quote: 'Nema labavo.',
           description: 'All around guy, into web dev, having fun and enjoying low iq music.',
           github: 'https://github.com/lpredova',
@@ -66,20 +66,20 @@ define([
         break;
         case 'zoran':
         data = {
-          name: 'Zoran',
-          surname: 'Antolovič',
-          hometown: 'Zadar',
-          company: 'Rentlio',
-          role: 'Backend/Frontend dev',
-          image: '/assets/img/zoran.jpg',
-          quote: 'Nema labavo.',
-          description: 'All around guy, into web dev, having fun and enjoying low iq music.',
-          github: 'https://github.com/lpredova',
-          twitter: 'https://twitter.com/lovro_p',
-          linkedin: 'https://hr.linkedin.com/in/lovropredovan',
-          education: [{name:'OS Bartula Kasica Zadar',years:'1999-2007'},{name:'Gymnasium of Franjo Petric Zadar',years:'2007-2011'},{name:'Faculty of organization and informatics Varazdin',years:'2011-2016'}],
-          skills:[{name:'PHP',level:'7.5'},{name:'Javascript',level:'5'},{name:'SQL',level:'8'},{name:'Python',level:'6'},{name:'CSS',level:'7.5'},{name:'Java',level:'-9000'}],
-        };
+         name: 'Zoran',
+         surname: 'Antolović',
+         hometown: 'Osijek',
+         company: 'Trikoder / MyBeeLine',
+         role: 'Backend Dev / Ops',
+         image: '/assets/img/zoran.jpg',
+         quote: 'To nikad nije ni radilo.',
+         description: 'Experienced developer, public speaker and semi-entepreneur, in love with technology and turbo folk',
+         github: 'https://github.com/zoka123',
+         twitter: 'https://twitter.com/zoran_antolovic',
+         linkedin: 'https://hr.linkedin.com/in/antoloviczoran',
+         education: [{name:'OS Jagode Truhelke Osijek',years:'1999-2007'},{name:'Klasična gimnazija u Osijeku',years:'2007-2011'},{name:'Faculty of Organization and Informatics Varaždin',years:'2011-2017'}],
+         skills:[{name:'PHP',level:'8.5'},{name:'Javascript',level:'5'},{name:'SQL',level:'8'},{name:'Bash',level:'6'},{name:'CSS',level:'9'},{name:'Java',level:'4'}],
+       };
         break;
         default:
         console.log(":)");
