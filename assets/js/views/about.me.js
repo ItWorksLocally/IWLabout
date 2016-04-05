a34db09d67e3061fe47451ce473a5850f9571ffc
Zoran Antolovic <zokadgo@gmail.com>
2016-04-05 17:21:45 +0200

    glup sam

--- assets/js/views/about.me.js
+++ assets/js/views/about.me.js
@@ -67,18 +67,18 @@ define([
         case 'zoran':
         data = {
           name: 'Zoran',
-          surname: 'Antolovič',
-          hometown: 'Zadar',
-          company: 'Rentlio',
-          role: 'Backend/Frontend dev',
+          surname: 'Antolović',
+          hometown: 'Osijek',
+          company: 'Trikoder / MyBeeLine',
+          role: 'Backend Dev / Ops',
           image: '/assets/img/zoran.jpg',
-          quote: 'Nema labavo.',
-          description: 'All around guy, into web dev, having fun and enjoying low iq music.',
-          github: 'https://github.com/lpredova',
-          twitter: 'https://twitter.com/lovro_p',
-          linkedin: 'https://hr.linkedin.com/in/lovropredovan',
-          education: [{name:'OS Bartula Kasica Zadar',years:'1999-2007'},{name:'Gymnasium of Franjo Petric Zadar',years:'2007-2011'},{name:'Faculty of organization and informatics Varazdin',years:'2011-2016'}],
-          skills:[{name:'PHP',level:'7.5'},{name:'Javascript',level:'5'},{name:'SQL',level:'8'},{name:'Python',level:'6'},{name:'CSS',level:'7.5'},{name:'Java',level:'-9000'}],
+          quote: 'To nikad nije ni radilo.',
+          description: 'Experienced developer, public speaker and semi-entepreneur, in love with technology and turbo folk',
+          github: 'https://github.com/zoka123',
+          twitter: 'https://twitter.com/zoran_antolovic',
+          linkedin: 'https://hr.linkedin.com/in/antoloviczoran',
+          education: [{name:'OS Jagode Truhelke Osijek',years:'1999-2007'},{name:'Klasična gimnazija u Osijeku',years:'2007-2011'},{name:'Faculty of Organization and Informatics Varaždin',years:'2011-2017'}],
+          skills:[{name:'PHP',level:'8.5'},{name:'Javascript',level:'5'},{name:'SQL',level:'8'},{name:'Bash',level:'6'},{name:'CSS',level:'9'},{name:'Java',level:'4'}],
         };
         break;
         default:
