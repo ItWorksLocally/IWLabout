88a47b4083ddbd3fae7eedb06d0d7a5c20b0ef83
Lovro Predovan <lovro.predovan@gmail.com>
2016-04-05 18:43:28 +0200

    Added igor and added padding to navigation elements

--- assets/js/views/about.me.js
+++ assets/js/views/about.me.js
@@ -32,19 +32,19 @@ define([
         break;
         case 'igor':
         data = {
-          name: 'Igor',
-          surname: 'Rinkovec',
-          hometown: 'Varaždin',
-          company: 'Trikoder',
-          role: 'Backend dev',
-          image: '/assets/img/igor.jpg',
-          quote: 'Nema labavo.',
-          description: 'All around guy, into web dev, having fun and enjoying low iq music.',
-          github: 'https://github.com/lpredova',
-          twitter: 'https://twitter.com/lovro_p',
-          linkedin: 'https://hr.linkedin.com/in/lovropredovan',
-          education: [{name:'OS Bartula Kasica Zadar',years:'1999-2007'},{name:'Gymnasium of Franjo Petric Zadar',years:'2007-2011'},{name:'Faculty of organization and informatics Varazdin',years:'2011-2016'}],
-          skills:[{name:'PHP',level:'7.5'},{name:'Javascript',level:'5'},{name:'SQL',level:'8'},{name:'Python',level:'6'},{name:'CSS',level:'7.5'},{name:'Java',level:'-9000'}],
+          name: "Igor",
+          surname: "Rinkovec",
+          hometown: "Varaždin",
+          company: "Trikoder",
+          role: "Backend Developer",
+          image: "/assets/img/igor.JPG",
+          quote: "Lovro, popravi ovo.",
+          description: "Loves breaking things in the weirdest ways possible and making computers learn things.",
+          github: "https://github.com/TheWildHorse",
+          twitter: "https://twitter.com/LordShigi",
+          linkedin: "https://hr.linkedin.com/in/igorrinkovec",
+          education: [{name:"Faculty Of Organization And Informatics",years:"2014-2019(expected)"}],
+          skills:[{name:"PHP",level:"8.9999999"},{name:"Javascript",level:"7"},{name:"SQL",level:"8"},{name:"Python",level:"6"},{name:"CSS",level:"7.5"},{name:"BrainF*ck",level:"++++++++++"}],
         };
         break;
         case 'lovro':
@@ -66,20 +66,20 @@ define([
         break;
         case 'zoran':
         data = {
-         name: 'Zoran',
-         surname: 'Antolović',
-         hometown: 'Osijek',
-         company: 'Trikoder / MyBeeLine',
-         role: 'Backend Dev / Ops',
-         image: '/assets/img/zoran.jpg',
-         quote: 'To nikad nije ni radilo.',
-         description: 'Experienced developer, public speaker and semi-entepreneur, in love with technology and turbo folk',
-         github: 'https://github.com/zoka123',
-         twitter: 'https://twitter.com/zoran_antolovic',
-         linkedin: 'https://hr.linkedin.com/in/antoloviczoran',
-         education: [{name:'OS Jagode Truhelke Osijek',years:'1999-2007'},{name:'Klasična gimnazija u Osijeku',years:'2007-2011'},{name:'Faculty of Organization and Informatics Varaždin',years:'2011-2017'}],
-         skills:[{name:'PHP',level:'8.5'},{name:'Javascript',level:'5'},{name:'SQL',level:'8'},{name:'Bash',level:'6'},{name:'CSS',level:'9'},{name:'Java',level:'4'}],
-       };
+          name: 'Zoran',
+          surname: 'Antolović',
+          hometown: 'Osijek',
+          company: 'Trikoder / MyBeeLine',
+          role: 'Backend Dev / Ops',
+          image: '/assets/img/zoran.jpg',
+          quote: 'To nikad nije ni radilo.',
+          description: 'Experienced developer, public speaker and semi-entepreneur, in love with technology and turbo folk',
+          github: 'https://github.com/zoka123',
+          twitter: 'https://twitter.com/zoran_antolovic',
+          linkedin: 'https://hr.linkedin.com/in/antoloviczoran',
+          education: [{name:'OS Jagode Truhelke Osijek',years:'1999-2007'},{name:'Klasična gimnazija u Osijeku',years:'2007-2011'},{name:'Faculty of Organization and Informatics Varaždin',years:'2011-2017'}],
+          skills:[{name:'PHP',level:'8.5'},{name:'Javascript',level:'5'},{name:'SQL',level:'8'},{name:'Bash',level:'6'},{name:'CSS',level:'9'},{name:'Java',level:'4'}],
+        };
         break;
         default:
         console.log(":)");
